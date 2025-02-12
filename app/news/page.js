a9458d4d1628354bf94fac9a0079bf6604bb9cad
rohxn-rai <rohan.rai932@outlook.com>
2025-02-12 10:30:45 -0500

    modified news page - added dynamic data instead of static data

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import { DUMMY_NEWS } from "@/dummy-news";
 
 const NewsPage = () => {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/first-news">first-news</Link>
-        </li>
-        <li>
-          <Link href="/news/secound-news">secound-news</Link>
-        </li>
-        <li>
-          <Link href="/news/third-news">third-news</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newItem) => (
+          <li key={newItem.id}>
+            <Link href={`/news/${newItem.slug}`}>
+              <img src={`/images/news/${newItem.image}`} alt={newItem.title} />
+              <span>{newItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
