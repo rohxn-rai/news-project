14f065ca43fad892bd78d748812f000ddaf6049b
rohxn-rai <rohan.rai932@outlook.com>
2025-02-12 10:12:23 -0500

    added main-header.js - added header items home and news to navigate

---
+++ components/main-header.js
@@ -0,0 +1,18 @@
+const { default: Link } = require("next/link");
+
+const MainHeader = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/news">News</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default MainHeader;
