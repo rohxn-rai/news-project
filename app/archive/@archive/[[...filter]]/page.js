1875c11a018cfd04c292a25b4617a68cec00643f
rohxn-rai <rohan.rai932@outlook.com>
2025-02-13 05:10:43 -0500

    added year and month filter - this page is for archive year and month section - removed the previous [year] for dynamic pages - added [[...filter]] for catch all pages and hence only one page.js under the archive parallel route

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+import NewsList from "@/components/news-list";
+
+import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+
+const FilteredNewsPage = ({ params }) => {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  const links = getAvailableNewsYears();
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => (
+              <li key={link}>
+                <Link href={`/archive/${link}`}>{link}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+};
+
+export default FilteredNewsPage;
