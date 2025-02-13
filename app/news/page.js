839a98b26b89ab335c4722728d6a0d779abaf992
rohxn-rai <rohan.rai932@outlook.com>
2025-02-13 04:30:25 -0500

    modified news page - removed the previously added news list and added the news list components

--- app/news/page.js
+++ app/news/page.js
@@ -1,22 +1,12 @@
-import Link from "next/link";
-import Image from "next/image";
-
 import { DUMMY_NEWS } from "@/dummy-news";
+
+import NewsList from "@/components/news-list";
 
 const NewsPage = () => {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newItem) => (
-          <li key={newItem.id}>
-            <Link href={`/news/${newItem.slug}`}>
-              <img src={`/images/news/${newItem.image}`} alt={newItem.title} />
-              <span>{newItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 };
