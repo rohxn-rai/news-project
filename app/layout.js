b416a8abe519bd6d42e3b8778cd1ca9b1f2f096d
rohxn-rai <rohan.rai932@outlook.com>
2025-02-13 08:32:15 -0500

    modified app layout - removed the the main header

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import MainHeader from "@/components/main-header";
 import "./globals.css";
 
 export const metadata = {
@@ -6,15 +5,12 @@ export const metadata = {
   description: "Learn how to route to different pages.",
 };
 
-export default function RootLayout({ children }) {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
-        <div id="page">
-          <MainHeader />
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
