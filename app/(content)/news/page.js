0429c5e1762506b8cabe05867d0b0ddb2bebcbde
rohxn-rai <rohan.rai932@outlook.com>
2025-02-14 08:38:44 -0500

    modified news page - change it back into a server component page. - removed the previous functions that would work as back-end - improved the back-end functionality - this improves the

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,49 +1,18 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 import NewsList from "@/components/news-list";
 
-const NewsPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setNews(news);
-      setIsLoading(false);
-    };
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
   }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 };
