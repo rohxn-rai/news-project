38d925caeae7564f68f40c7ed1214b36f29aeff3
rohxn-rai <rohan.rai932@outlook.com>
2025-02-14 08:16:20 -0500

    modified news page - added back-end functionality - removed the previous static data fetching from a json file

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,49 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
+import { useEffect, useState } from "react";
 
 import NewsList from "@/components/news-list";
 
 const NewsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setNews(news);
+      setIsLoading(false);
+    };
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 };
