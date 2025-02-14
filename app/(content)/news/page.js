207214dd396647892126eb1b59c9bc318767ad90
rohxn-rai <rohan.rai932@outlook.com>
2025-02-14 08:47:56 -0500

    modified news page - removed the fuctions - added the function from functionality package

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,14 +1,9 @@
 import NewsList from "@/components/news-list";
 
+import { getAllNews } from "@/lib/news";
+
 const NewsPage = async () => {
-  const response = await fetch("http://localhost:8080/news");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch news.");
-  }
-
-  const news = await response.json();
-
+  const news = getAllNews();
   return (
     <>
       <h1>News Page</h1>
